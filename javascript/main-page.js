de29fe8960b8bbc310a106c148b5ef26020a7185
Mackendrix <divineojaruvwee@gmail.com>
2026-09-14 15:55:12 +0100

    Added the javascript Product functionality

--- javascript/main-page.js
+++ javascript/main-page.js
@@ -1,15 +1,39 @@
+// List of products
+const products = [
+    {
+        name:"Hoodie",
+        price:25000,
+        image:"../images/clothes-cutout.png",
+        category:"Clothes"
+    },
+     {
+      name: "Headphones",
+      price: 18000,
+      image: "../images/Gadgets-cutout.png",
+      category: "Gadgets"
+    },
+    {
+      name: "Laptop",
+      price: 450000,
+      image: "../images/Laptops_edited.png",
+      category: "Electronics"
+    }
+];
+// Fucntion to disply items
+const productContainer = document.getElementById("products");
 
-  const images = [
-    "phones_edited.png",
-    "phones_edited.png",
-    "phones_edited.png"
-  ];
-
-  const gallery = document.getElementById("gallery");
-
-  images.forEach((file) => {
-    const img = document.createElement("img");
-    img.src = `images/${file}`;
-    img.alt = file;
-    gallery.appendChild(img);
-  });
+products.forEach((product) => {
+    productContainer.innerHTML += `
+        <div class = product-card>
+            <div class = "img-container">
+            <img src = "${product.image}" alt = "${product.name}">
+            </div>
+            <div class = "info">
+            <h3>${product.name}</h3>
+            <p>${product.category}</p>
+            <strong>₦${product.price.toLocaleString()}</strong>
+            <button id = "add-item" style = "display: block">Add to Cart</button>
+            </div>
+        </div>
+    `
+});
